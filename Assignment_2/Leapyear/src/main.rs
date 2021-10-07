59c30f2a6e33b8fd10f9da694ec26f0d335b2434
Mayankrajput70 <88483601+Mayankrajput70@users.noreply.github.com>
2021-10-07 11:41:55 +0530

    Update main.rs

--- Assignment_2/Leapyear/src/main.rs
+++ Assignment_2/Leapyear/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
     let years_arr: [i32; 11] = [2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031];
     let mut count = 0;
-    for i in 0..11 {
-        if years_arr[i] % 4 == 0 {
-            if years_arr[i] % 100 == 0 {
-                if years_arr[i] % 400 == 0 {
+    for element in 0..11 {
+        if years_arr[element] % 4 == 0 {
+            if years_arr[element] % 100 == 0 {
+                if years_arr[element] % 400 == 0 {
                     count = count + 1;
                 }
                 else { }
