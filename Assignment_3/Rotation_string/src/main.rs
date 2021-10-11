ef77be07192da0594e582e24fc8ed58b9f2120bd
Mayank <mayank.rajput@knoldus.com>
2021-10-11 11:45:02 +0530

    Update main.rs

--- Assignment_3/Rotation_string/src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let string = String::from("mayank");
-    let replace_from = "mayank";
-    let replace_to = "knayam";
-    let result = string.replace(replace_from, replace_to);
-    println!("{} converted {}.", string, result);
-}
