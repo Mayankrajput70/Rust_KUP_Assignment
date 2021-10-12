45ebfe4c344d760be98316f11381c15ad945195f
Mayank <mayank.rajput@knoldus.com>
2021-10-12 23:46:35 +0530

    Update main.rs

---
+++ Assignment_4/Complex_Number/src/main.rs
@@ -0,0 +1,28 @@
+struct Numbers {
+    num_first: u32,
+    num_second: u32,
+}
+fn main() {
+    let sum = Numbers {
+        num_first: 100,
+        num_second: 50.0 as u32,
+    };
+    println!("Addition of two numbers is {} ", sum.addition());
+    println!("Subtraction of two numbers is: {}", sum.subtract());
+    println!("Multiplication of two numbers is: {}", sum.multiplication());
+    println!("Divisionn of two numbers is: {}", sum.division());
+}
+impl Numbers {
+    fn addition(&self) -> u32 {
+        self.num_first + self.num_second
+    }
+    fn subtract(&self) -> u32 {
+        self.num_first - self.num_second
+    }
+    fn multiplication(&self) -> u32 {
+        self.num_first * self.num_second
+    }
+    fn division(&self) -> u32 {
+        self.num_first / self.num_second
+    }
+}
